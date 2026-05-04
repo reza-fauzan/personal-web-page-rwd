72d0264f9427c5c012c754ff556036c57d51414c
Reza Fauzan Pratama <rezafauzanpratama01@email.com>
2026-05-04 19:58:35 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div>
+    </div>
+</div>
---
+++ main.php
@@ -0,0 +1,4 @@
+<div class="col-md-9">
+<h1>main</h1>
+</div>
+</div>
